6a55c59b790d2a5715c02c7e56bc7ef03442b30d
Jay Padaliya <developer.padaliyajay@gmail.com>
2024-07-02 15:09:25 +0000

    fixed audio filter

--- bigbluebutton-html5/imports/ui/components/audio/audio-filter/component.jsx
+++ bigbluebutton-html5/imports/ui/components/audio/audio-filter/component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { defineMessages, injectIntl } from 'react-intl';
 import Styled from './styles';
-import Settings from '/imports/ui/services/settings';
 import ApplicationMenu from '/imports/ui/components/settings/submenus/application/component';
 
 const propTypes = {
@@ -11,6 +10,9 @@ const propTypes = {
   }).isRequired,
   isAudioConnected: PropTypes.bool.isRequired,
   isListenOnly: PropTypes.bool.isRequired,
+  updateSettings: PropTypes.func.isRequired,
+  application: PropTypes.object.isRequired,
+  transcription: PropTypes.object.isRequired,
 };
 
 const intlMessages = defineMessages({
@@ -22,31 +24,49 @@ const intlMessages = defineMessages({
     id: 'app.audioFilter.audioFilterDisabledButtonText',
     description: 'Turn off original sound',
   },
+  audioFilterEnabledAlertText: {
+    id: 'app.audioFilter.audioFilterEnabledAlertText',
+    description: 'Original sound turned on',
+  },
+  audioFilterDisabledAlertText: {
+    id: 'app.audioFilter.audioFilterDisabledAlertText',
+    description: 'Original sound turned off',
+  },
 });
 
 class AudioFilter extends React.Component {
   constructor(props) {
     super(props);
 
+    const { application } = this.props;
+
     this.state = {
       audioFilterEnabled: ApplicationMenu.isAudioFilterEnabled(
-        Settings.application.microphoneConstraints,
+        application.microphoneConstraints,
       ),
     };
 
+    this.updateSettings = props.updateSettings;
     this.handleAudioFilterToggle = this.handleAudioFilterToggle.bind(this);
   }
 
   handleAudioFilterToggle() {
     const { audioFilterEnabled } = this.state;
+    const { application, transcription } = this.props;
 
     const _newConstraints = {
       autoGainControl: !audioFilterEnabled,
       echoCancellation: !audioFilterEnabled,
       noiseSuppression: !audioFilterEnabled,
     };
-    Settings.application.microphoneConstraints = _newConstraints;
-    Settings.save();
+
+    this.updateSettings({
+      transcription,
+      application: {
+        ...application,
+        microphoneConstraints: _newConstraints,
+      },
+    }, audioFilterEnabled ? intlMessages.audioFilterEnabledAlertText : intlMessages.audioFilterDisabledAlertText);
 
     this.setState({ audioFilterEnabled: !audioFilterEnabled });
   }
--- bigbluebutton-html5/imports/ui/components/audio/audio-filter/container.jsx
+++ bigbluebutton-html5/imports/ui/components/audio/audio-filter/container.jsx
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import AudioFilter from './component';
 import Service from '../service';
+import SettingsService from '/imports/ui/services/settings';
+import { updateSettings } from '/imports/ui/components/settings/service';
 
 class AudioFilterContainer extends PureComponent {
   render() {
@@ -14,4 +16,7 @@ class AudioFilterContainer extends PureComponent {
 export default withTracker(() => ({
   isAudioConnected: Service.isConnected(),
   isListenOnly: Service.isListenOnly(),
+  updateSettings,
+  application: SettingsService.application,
+  transcription: SettingsService.transcription,
 }))(AudioFilterContainer);
